457483a44d5db9acc828c485b11af91a35b52d0d
Savvas Sampaziotis <savvas456pentium@gmail.com>
2018-02-04 21:02:13 +0200

    Queries 1-8 and 10 added

--- queries.sql
+++ queries.sql
@@ -1,17 +1,60 @@
 
 # 1
-select * FROM album NATURAL JOIN included_in;
+SELECT * FROM album NATURAL JOIN included_in;
 
-#3
-select debut, band.artist_name
+# 2
+SELECT distinct has_created.artist_name from has_created;
+
+# 3
+SELECT band.artist_name, band.founding_date, artist.debut, band.split_date
  FROM artist 
- INNER JOIN band ON band.artist_name=artist.artist_name 
+ NATURAL JOIN band
  ORDER BY debut LIMIT 5;
 
-#5 XXXXXXXXXXXXXXXXXXXXX
+# 4
+SELECT  included_in.album_title, SUM(song.duration) AS album_duration 
+	FROM song
+	NATURAL JOIN included_in 
+	GROUP BY included_in.album_title;
 
-#7
-#select * from included_in group by included_in
+# 5 
+SELECT  included_in.album_title, SUM(song.duration) AS album_duration 
+	FROM song
+	NATURAL JOIN included_in 
+	GROUP BY included_in.album_title
+	HAVING count(song.song_title) > 2;
+	
+# 6 
+SELECT * FROM song WHERE song.duration > (SELECT AVG(song.duration) FROM song);
 
+# 7 # EXCEPT is'n supported. So we use NOT IN.
+SELECT  artist.artist_name
+	FROM artist
+	WHERE artist.artist_name NOT IN ( SELECT DISTINCT has_created.artist_name FROM has_created );
 
+# 8 INTERSECT is not supported, so we use inner join.
+# This selects the song titles that are registered in albums and are linked to an artist as well
+SELECT has_created.song_title, included_in.album_title, has_created.artist_name
+	FROM has_created
+	INNER JOIN included_in ON included_in.song_title=has_created.song_title;
 
+# 9 
+SELECT  artist.artist_name
+	FROM artist
+	WHERE artist.artist_name NOT IN ( SELECT DISTINCT has_created.artist_name FROM has_created );
+
+	
+# 10 
+CREATE OR REPLACE VIEW album_duration 
+	AS SELECT artist.debut, band.logo, band.founding_date, band.split_date
+	FROM artist 
+	NATURAL JOIN band
+	ORDER BY debut;
+
+SELECT * FROM album_duration;
+	
+	
+	
+	
+	
+	
